f51a9da7794c71cd1004072316e01cac4f855ad6
MokshShah1 <147677786+MokshShah1@users.noreply.github.com>
2024-02-15 22:44:23 -0600

    Update predictor.py

--- predictor.py
+++ predictor.py
@@ -10,13 +10,11 @@ def create_pie_chart(prediction_probabilities):
               'No Pancreatic Cancer, but Disrupted Circadian Rhythm',
               'No Pancreatic Cancer and Regular Circadian Rhythm',
               'Pancreatic Cancer but Regular Circadian Rhythm']
-    colors = ['#8A2BE2', '#4169E1', '#00BFFF', '#87CEEB']  # Purple and shades of blue
+    colors = ['#ff9999', '#66b3ff', '#99ff99', '#ffcc99']
     fig1, ax1 = plt.subplots()
-    filtered_probabilities = [p for p in prediction_probabilities if p != 0]  # Exclude 0 values
-    filtered_labels = [labels[i] for i, p in enumerate(prediction_probabilities) if p != 0]
-    ax1.pie(filtered_probabilities, colors=colors, labels=filtered_labels, autopct='%1.1f%%', startangle=140)
+    ax1.pie(prediction_probabilities, colors=colors, labels=labels, autopct='%1.1f%%', startangle=140)
     ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-    return fig1
+    st.pyplot(fig1)
 
 # Load and predict model function
 def load_and_predict_model(test_ratios, selected_option):
@@ -34,21 +32,13 @@ def load_and_predict_model(test_ratios, selected_option):
         predictions = model.predict(test_ratios)
         prediction_probabilities = model.predict_proba(test_ratios)
 
-        prediction_labels = {
-            'pancreatic_circadian': 'Pancreatic Cancer and Disrupted Circadian Rhythm',
-            'no_pancreatic_circadian': 'No Pancreatic Cancer, but Disrupted Circadian Rhythm',
-            'no_pancreatic_no_circadian': 'No Pancreatic Cancer and Regular Circadian Rhythm',
-            'pancreatic_no_circadian': 'Pancreatic Cancer but Regular Circadian Rhythm'
-        }
-
-        st.markdown('<div class="predicted-scenario">Predicted Scenario:</div>', unsafe_allow_html=True)
+        st.markdown('<div class="predicted-scenario" style="color: purple;">Predicted Scenario:</div>', unsafe_allow_html=True)
         for i, class_name in enumerate(model.classes_):
             transformed_class_name = prediction_labels[class_name]
-            st.markdown(f'<div class="predicted-scenario">{transformed_class_name}: {prediction_probabilities[0][i]}</div>', unsafe_allow_html=True)
+            st.write(f"{transformed_class_name}: {prediction_probabilities[0][i]}")
 
         # Create and display pie chart
-        fig = create_pie_chart(prediction_probabilities[0])
-        st.pyplot(fig)
+        create_pie_chart(prediction_probabilities[0])
 
     else:
         st.error(f"Model file '{model_file_path}' not found.")
@@ -57,7 +47,7 @@ def load_and_predict_model(test_ratios, selected_option):
 def main():
     st.markdown('<link rel="stylesheet" type="text/css" href="circsync_css.css">', unsafe_allow_html=True) # Link the CSS file
     
-    st.markdown('<h1 class="centered-title">Circadian Sync</h1>', unsafe_allow_html=True)
+    st.markdown('<h1 class="centered-title" style="color: purple;">Circadian Sync</h1>', unsafe_allow_html=True)
 
     st.markdown('<div class="main-content">CircadianSync is a Machine Learning model that intakes the gene expression levels of patients in order to analyze and predict whether they have pancreatic adenocarcinoma, circadian dysfunction, neither, or both.</div>', unsafe_allow_html=True)
 
