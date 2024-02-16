19ff2ddccb15a4c79dcf570c8aa5f1397ba1f914
MokshShah1 <147677786+MokshShah1@users.noreply.github.com>
2024-02-15 23:09:10 -0600

    Update predictor.py

--- predictor.py
+++ predictor.py
@@ -41,82 +41,64 @@ def load_and_predict_model(test_ratios, selected_option):
             'pancreatic_no_circadian': 'Pancreatic Cancer but Regular Circadian Rhythm'
         }
 
-        st.markdown('<style>' +
-                    '.container { display: flex; }' +
-                    '.main-content { flex: 1; background-color: #333333; border-radius: 10px; padding: 20px; margin-right: 20px; }' +
-                    '.sidebar-content { width: 300px; background-color: #333333; border-radius: 10px; padding: 20px; }' +
-                    '.centered-title { text-align: center; margin-bottom: 20px; }' +
-                    '.predicted-scenario { font-weight: bold; color: #8A2BE2; font-size: 16px; margin-bottom: 10px; }' +
-                    '.prediction-probabilities { font-style: italic; color: #8A2BE2; }' +
-                    '.predict-button { background-color: #4CAF50; color: white; padding: 10px 20px; border: none; border-radius: 5px; cursor: pointer; }' +
-                    '.predict-button:hover { background-color: #45a049; }' +
-                    '#pie-chart-container { width: 400px; margin-top: 20px; margin-left: auto; margin-right: 0; }' +
-                    '</style>', unsafe_allow_html=True)
+        st.markdown('<div class="predicted-scenario">Predicted Scenario:</div>', unsafe_allow_html=True)
+        for i, class_name in enumerate(model.classes_):
+            transformed_class_name = prediction_labels[class_name]
+            st.markdown(f'<div class="predicted-scenario">{transformed_class_name}: {prediction_probabilities[0][i]}</div>', unsafe_allow_html=True)
 
-        st.markdown('<h1 class="centered-title">Circadian Sync</h1>', unsafe_allow_html=True)
+        # Create and display pie chart
+        fig = create_pie_chart(prediction_probabilities[0])
+        st.pyplot(fig)
 
-        st.markdown('<div class="container">', unsafe_allow_html=True)
-
-        st.markdown('<div class="main-content">CircadianSync is a Machine Learning model that intakes the gene expression levels of patients in order to analyze and predict whether they have pancreatic adenocarcinoma, circadian dysfunction, neither, or both.</div>', unsafe_allow_html=True)
-
-        st.sidebar.title("CircSync Predictor")
-        st.sidebar.markdown('<div class="sidebar-content">To use CircSync to predict a patient\'s diagnosis, upload an Excel file with their gene expression levels as numerical values in it. Make sure there are two columns: one for gene names labeled GeneID and one for the values labeled ExpressionLevels</div>', unsafe_allow_html=True)
-
-        uploaded_file = st.sidebar.file_uploader("Upload a file", type=["xlsx"])
-
-        if uploaded_file is not None:
-            df = pd.read_excel(uploaded_file, header=None)
-            df.to_excel("imported_data.xlsx", index=False)
-            df.to_excel("modified_file.xlsx", index=False)
-
-            def load_test_data(file_path):
-                df = pd.read_excel(file_path)
-                return df
-
-            def preprocess_test_data(df):
-                df = df.apply(pd.to_numeric, errors='coerce')
-                df.fillna(0, inplace=True)
-                return df
-
-            def calculate_ratios(df):
-                file_sum = df.values.sum()
-                file_ratios = df.values / file_sum
-                flattened_ratios = file_ratios.flatten()
-                return flattened_ratios.reshape(1, -1)
-
-            file_path = 'modified_file.xlsx'
-
-            if os.path.exists(file_path) and file_path.endswith('.xlsx'):
-                test_data = load_test_data(file_path)
-                test_data = preprocess_test_data(test_data)
-                test_ratios = calculate_ratios(test_data)
-
-                selected_option = st.sidebar.selectbox("Select a model", ["Random Forest", "Gradient Boosting Classifier", "K Neighbors", "Decision Tree Classifier"])
-
-                if selected_option:
-                    model = joblib.load(model_file_path)
-                    predictions = model.predict(test_ratios)
-                    prediction_probabilities = model.predict_proba(test_ratios)
-
-                    prediction_labels = {
-                        'pancreatic_circadian': 'Pancreatic Cancer and Disrupted Circadian Rhythm',
-                        'no_pancreatic_circadian': 'No Pancreatic Cancer, but Disrupted Circadian Rhythm',
-                        'no_pancreatic_no_circadian': 'No Pancreatic Cancer and Regular Circadian Rhythm',
-                        'pancreatic_no_circadian': 'Pancreatic Cancer but Regular Circadian Rhythm'
-                    }
-
-                    st.markdown('<div class="predicted-scenario">Predicted Scenario:</div>', unsafe_allow_html=True)
-                    for i, class_name in enumerate(model.classes_):
-                        transformed_class_name = prediction_labels[class_name]
-                        st.markdown(f'<div class="predicted-scenario">{transformed_class_name}: {prediction_probabilities[0][i]}</div>', unsafe_allow_html=True)
-
-                    # Create and display pie chart
-                    fig = create_pie_chart(prediction_probabilities[0])
-                    st.pyplot(fig)
+    else:
+        st.error(f"Model file '{model_file_path}' not found.")
 
 # Main function
 def main():
-    pass
+    st.markdown('<link rel="stylesheet" type="text/css" href="circsync_css.css">', unsafe_allow_html=True) # Link the CSS file
+    
+    st.markdown('<h1 class="centered-title">Circadian Sync</h1>', unsafe_allow_html=True)
+
+    st.markdown('<div class="container">', unsafe_allow_html=True)
+
+    st.markdown('<div class="main-content">CircadianSync is a Machine Learning model that intakes the gene expression levels of patients in order to analyze and predict whether they have pancreatic adenocarcinoma, circadian dysfunction, neither, or both.</div>', unsafe_allow_html=True)
+
+    st.sidebar.title("CircSync Predictor")
+    st.sidebar.markdown('<div class="sidebar-content">To use CircSync to predict a patient\'s diagnosis, upload an Excel file with their gene expression levels as numerical values in it. Make sure there are two columns: one for gene names labeled GeneID and one for the values labeled ExpressionLevels</div>', unsafe_allow_html=True)
+
+    uploaded_file = st.sidebar.file_uploader("Upload a file", type=["xlsx"])
+
+    if uploaded_file is not None:
+        df = pd.read_excel(uploaded_file, header=None)
+        df.to_excel("imported_data.xlsx", index=False)
+        df.to_excel("modified_file.xlsx", index=False)
+
+        def load_test_data(file_path):
+            df = pd.read_excel(file_path)
+            return df
+
+        def preprocess_test_data(df):
+            df = df.apply(pd.to_numeric, errors='coerce')
+            df.fillna(0, inplace=True)
+            return df
+
+        def calculate_ratios(df):
+            file_sum = df.values.sum()
+            file_ratios = df.values / file_sum
+            flattened_ratios = file_ratios.flatten()
+            return flattened_ratios.reshape(1, -1)
+
+        file_path = 'modified_file.xlsx'
+
+        if os.path.exists(file_path) and file_path.endswith('.xlsx'):
+            test_data = load_test_data(file_path)
+            test_data = preprocess_test_data(test_data)
+            test_ratios = calculate_ratios(test_data)
+
+            selected_option = st.sidebar.selectbox("Select a model", ["Random Forest", "Gradient Boosting Classifier", "K Neighbors", "Decision Tree Classifier"])
+
+            if selected_option:
+                load_and_predict_model(test_ratios, selected_option)
 
 if __name__ == "__main__":
     main()
